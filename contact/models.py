770fab5f0111e705e0ea9b93270ab2219f76bf08
Ruben S. Melo <pinguim6970@gmail.com>
2026-01-16 20:23:13 -0300

    Registrando e customizando o model

--- contact/models.py
+++ contact/models.py
@@ -3,8 +3,11 @@ from django.utils import timezone
 
 # Create your models here.
 class Contact(models.Model):
+    def __str__(self):
+        return f'{self.first_name} | {self.last_name} | {self.phone} | {self.email}'
+    
     first_name = models.CharField(max_length=50)
-    lasst_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=250, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
