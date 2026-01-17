58f96cae7473141f72a77d34ba4f2e43e1708039
Ruben S. Melo <pinguim6970@gmail.com>
2026-01-16 21:03:39 -0300

    Criação de imageField confg. MEDIA_ROOT e MEDIA_URL

--- agenda/settings.py
+++ agenda/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
--- contact/models.py
+++ contact/models.py
@@ -12,5 +12,7 @@ class Contact(models.Model):
     email = models.EmailField(max_length=250, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
 
 
