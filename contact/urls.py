fb1d26ec69620485c5d2f414d82fb33aa5f92390
Ruben S. Melo <pinguim6970@gmail.com>
2026-01-22 17:14:17 -0300

    Até a aula 476

--- contact/urls.py
+++ contact/urls.py
@@ -7,4 +7,5 @@ app_name = 'contact'
 urlpatterns = [
     path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
     path('', views.index, name='index'),  # type:ignore
+    path('search/', views.search, name='search'),  # type:ignore
 ]
